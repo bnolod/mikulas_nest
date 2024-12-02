40410418bd11358463a45256989ca4a31b9781e9
bnolod <sadfoolc@gmail.com>
2024-12-02 13:08:35 +0100

    schema fix + seed

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -3,20 +3,30 @@ import { faker } from '@faker-js/faker'
 const prisma = new PrismaClient()
 async function main() {
     for (let n = 1; n < 101; n++) {
-        await prisma.child.create({
+        const child = await prisma.child.create({
             data: {
-                name: faker.person.fullName()
-                address: faker.location.county()+"," ,
-                behaved: true
+                name: faker.person.fullName(),
+                address: faker.location.county()+","+faker.location.streetAddress() ,
+                behaved: faker.datatype.boolean()
             }
         })
         await prisma.toy.create({
             data: {
-                name : `Játék${n}`,
+                name : faker.color.human()+" "+faker.animal.type(),
                 material: "other",
+                weight: faker.number.float({min:0,max:10,multipleOf:0.25})
 
             }
         })
+        if (child.behaved) {
+            await prisma.childrenToToys.create({
+                data: {
+                    child_id:child.id,
+                    toy_id: faker.number.int({min:1,max:n})
+                }
+            })
+        }
+
     }
     }
     main()
